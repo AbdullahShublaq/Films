460613d1be1de3b08e6c0b2cd79cbbb7169d28ed
Abdullah Shublaq <abd.shublaq@gmail.com>
2020-06-11 21:34:54 +0300

    add real vedio to films seeder

--- database/seeds/FilmSeeder.php
+++ database/seeds/FilmSeeder.php
@@ -19,7 +19,7 @@ class FilmSeeder extends Seeder
                 'overview' => 'film1film1film1film1film1film1film1film1film1film1film1film1',
                 'background_cover' => 'film_background_covers/film-bg.jpg',
                 'poster' => 'film_posters/film-poster.jpg',
-                'url' => '<div itemscope itemtype="https://schema.org/VideoObject"><meta itemprop="uploadDate" content="Wed Mar 04 2020 09:25:08 GMT+0200 (توقيت شرق أوروبا الرسمي)"/><meta itemprop="name" content="Naruto Shippuden Op 4"/><meta itemprop="duration" content="PT1M33.205S" /><meta itemprop="thumbnailUrl" content="https://content.jwplatform.com/thumbs/g7796nVg-1280.jpg"/><meta itemprop="contentUrl" content="https://content.jwplatform.com/videos/g7796nVg-zJl9Il4I.mp4"/><script src="https://cdn.jwplayer.com/players/g7796nVg-mBCrmi1a.js"></script></div>',
+                'url' => '<div itemscope itemtype="https://schema.org/VideoObject"><meta itemprop="uploadDate" content="Thu Jun 11 2020 21:21:24 GMT+0300 (توقيت شرق أوروبا الصيفي)"/><meta itemprop="name" content="Ip Man 4 (2019) Official Us Theatrical Trailer   Donnie Yen, Scott Adkins &amp; Danny Chan As Bruce Lee (1)"/><meta itemprop="duration" content="PT1M45.164S" /><meta itemprop="thumbnailUrl" content="https://content.jwplatform.com/thumbs/hgmsBKyV-1280.jpg"/><meta itemprop="contentUrl" content="https://content.jwplatform.com/videos/hgmsBKyV-zJl9Il4I.mp4"/><script src="https://cdn.jwplayer.com/players/hgmsBKyV-mBCrmi1a.js"></script></div>',
             ],
             [
                 'name' => 'film2',
@@ -27,7 +27,7 @@ class FilmSeeder extends Seeder
                 'overview' => 'film2film2film2film2film2film2film2film2film2film2film2film2',
                 'background_cover' => 'film_background_covers/film-bg.jpg',
                 'poster' => 'film_posters/film-poster.jpg',
-                'url' => '<div itemscope itemtype="https://schema.org/VideoObject"><meta itemprop="uploadDate" content="Wed Mar 04 2020 09:25:08 GMT+0200 (توقيت شرق أوروبا الرسمي)"/><meta itemprop="name" content="Naruto Shippuden Op 4"/><meta itemprop="duration" content="PT1M33.205S" /><meta itemprop="thumbnailUrl" content="https://content.jwplatform.com/thumbs/g7796nVg-1280.jpg"/><meta itemprop="contentUrl" content="https://content.jwplatform.com/videos/g7796nVg-zJl9Il4I.mp4"/><script src="https://cdn.jwplayer.com/players/g7796nVg-mBCrmi1a.js"></script></div>',
+                'url' => '<div itemscope itemtype="https://schema.org/VideoObject"><meta itemprop="uploadDate" content="Thu Jun 11 2020 21:21:24 GMT+0300 (توقيت شرق أوروبا الصيفي)"/><meta itemprop="name" content="Ip Man 4 (2019) Official Us Theatrical Trailer   Donnie Yen, Scott Adkins &amp; Danny Chan As Bruce Lee (1)"/><meta itemprop="duration" content="PT1M45.164S" /><meta itemprop="thumbnailUrl" content="https://content.jwplatform.com/thumbs/hgmsBKyV-1280.jpg"/><meta itemprop="contentUrl" content="https://content.jwplatform.com/videos/hgmsBKyV-zJl9Il4I.mp4"/><script src="https://cdn.jwplayer.com/players/hgmsBKyV-mBCrmi1a.js"></script></div>',
             ]
         ];
 
